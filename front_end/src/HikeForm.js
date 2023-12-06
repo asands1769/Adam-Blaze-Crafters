3d753f7dd9aac55f10ddd8c34d4f1479c1de08c0
Braden E <bradene0@outlook.com>
2023-12-05 19:00:51 -0600

    Added better styling with flexbox

--- front_end/src/HikeForm.js
+++ front_end/src/HikeForm.js
@@ -1,14 +1,19 @@
 // HikeForm.js
 import React, { useState, useEffect } from 'react';
+import './historystyles.css';
 
 const HikeForm = ({ onSubmit, selectedHike, onEdit }) => {
   const [name, setName] = useState('');
   const [location, setLocation] = useState('');
+  const [date, setDate] = useState('');
+  const [notes, setNotes] = useState('');
 
   useEffect(() => {
     if (selectedHike) {
       setName(selectedHike.name);
       setLocation(selectedHike.location);
+      setDate(selectedHike.date);
+      setNotes(selectedHike.notes);
     }
   }, [selectedHike]);
 
@@ -16,25 +21,36 @@ const HikeForm = ({ onSubmit, selectedHike, onEdit }) => {
     e.preventDefault();
 
     if (selectedHike) {
-      onEdit({ ...selectedHike, name, location });
+      onEdit({ ...selectedHike, name, location, date, notes });
     } else {
-      onSubmit({ name, location });
+      onSubmit({ name, location, date, notes });
     }
 
+    // Reset form fields
     setName('');
     setLocation('');
+    setDate('');
+    setNotes('');
   };
 
   return (
     <form onSubmit={handleSubmit}>
       <h2>{selectedHike ? 'Edit Hike' : 'Add Hike'}</h2>
       <label>
-        Name:
-        <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+        Trail Name:
+        <input type="text" value={name} onChange={(e) => setName(e.target.value)} required />
       </label>
       <label>
-        Location:
-        <input type="text" value={location} onChange={(e) => setLocation(e.target.value)} />
+        Trail Location:
+        <input type="text" value={location} onChange={(e) => setLocation(e.target.value)} required />
+      </label>
+      <label>
+        Date of Hike:
+        <input type="date" value={date} onChange={(e) => setDate(e.target.value)} required />
+      </label>
+      <label>
+        Notes:
+        <textarea value={notes} onChange={(e) => setNotes(e.target.value)} />
       </label>
       <button type="submit">{selectedHike ? 'Update Hike' : 'Add Hike'}</button>
     </form>
