180f91af4c6b86f45befc1841f50e02043f5240c
Braden E <bradene0@outlook.com>
2023-12-11 16:59:58 -0600

    Trying to compile

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/nav/Header.js';
 import Home from './components/pages/Home.js'
-import Parks from './components/pages/Parks.js';
+//import Parks from './components/pages/Parks.js';
 import Animals from './components/pages/Animals.js';
 import Plants from './components/pages/Plants.js';
 import Weather from './components/pages/Weather.js';
