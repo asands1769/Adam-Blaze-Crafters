e32db4276c92f66b61bb9474936ec66d0b083b2d
Dustin Dev <94726570+dustinFriesenDev@users.noreply.github.com>
2023-12-20 15:55:32 -0500

    add checkbox, a select trip dropdown, save button, and a create trip link

--- front_end/src/components/pages/plants/Plants.js
+++ front_end/src/components/pages/plants/Plants.js
@@ -26,10 +26,9 @@ const Home = () => {
    const change = event => {setVal(event.target.value)}
    const clicked = (event) => {
      setClick(event.target.id);
-     
    }
-
-    const searchItems = data.filter(post => {
+    
+   const searchItems = data.filter(post => {
       if(val === ''){
         return post;
       } else if (post.scientificName.toLowerCase().includes(val.toLowerCase()) || post.commonName.toLowerCase().includes(val.toLowerCase())){
@@ -40,16 +39,18 @@ const Home = () => {
     const displaySearchedItems = searchItems.map(post => {
       return (
       <div key={post.id}>
-        <button id={post.id} onClick={clicked} className='btn-plants'>{`${post.scientificName} (${post.commonName})`}</button>
+        <form>
+          <input type="checkbox" id={post.id} name={post.id} value={post.scientificName}/>
+          <label id={post.id} onClick={clicked} className='btn-plants'>{`${post.scientificName} (${post.commonName})`}</label>
+        </form>
       </div>
       )
     })
-
+// methods for displaying information of plant within plant-container
     const clickedItems = data.filter(post => {
       if(post.id == click){
         return post;
       }
-      
     })
 
     const displayClickedItems = clickedItems.map(post => {
@@ -60,7 +61,6 @@ const Home = () => {
             <p>{`Scientific Name: ${post.scientificName}`}</p>
             <p>Family: {post.family}</p>
             <p>States found in: {post.currentDistribution}</p>
-            {/* {if (post.FederalListingStatus)} */}
             <p>Federal listing status: {post.federalListingStatus}</p>
           </div>
           <img className="plant-img-size" src={placeholderImage} alt="a plant sprouting with four leaves"/>
@@ -68,25 +68,32 @@ const Home = () => {
     })
 
   
-
- 
-
-    
-
-
   return (
       <div>
           <h2>Plants of Missouri</h2>
-        <div className='row'>
-          <div className='search-container'>
-          <div className='small'><small>*List is not a comprehensive list of plants in Missouri</small></div>
-          <div className='small'><small>**Data provided by <a href='https://ecos.fws.gov/ecp0/reports/ad-hoc-species-report-input'>U.S Fish & Wildlife Service: ECOS</a></small></div>
-          <input className="search-plants" onChange={change} placeholder="Find a plant by scientific or common name"/>
-          <div className='sidebar-size'>
-            {displaySearchedItems}
+        <div className='row main-container'>
+          <div className='leftside-container'>
+            <div className='save-container'>
+              <select>
+                <option>Select a Trip</option>
+                <option value='post.id1'>Trip 1</option>
+              </select>
+              <button type='submit' className='save-btn'>Save</button>
+            </div>
+            <div className='search-container'>
+            <div className='sidebar-size'>
+              <input className="search-plants" onChange={change} placeholder="Find a plant by scientific or common name"/>
+              {displaySearchedItems}
+              </div>
+              <div className='small'>
+                <small>*List is not a comprehensive of all plants in Missouri</small>
+              </div>
+              <div className='small'>
+                <small>**Data provided by <a href='https://ecos.fws.gov/ecp0/reports/ad-hoc-species-report-input'>U.S Fish & Wildlife Service: ECOS</a></small>
+              </div>
+            </div>
           </div>
-          </div>
-          <div id='plant-toggle' className='plant-container flex'>
+          <div className='plant-container flex'>
             {displayClickedItems}
           </div>
         </div>
